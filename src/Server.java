79497618bd0521f7ac4a4ba8ad4594c6b0ac6feb
daria smirnova12524 <d.smirnova@lab244.ru>
2024-06-09 19:42:29 +0300

    first commit: created a project

---
+++ src/Server.java
@@ -0,0 +1,5 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Server {
+
+}
