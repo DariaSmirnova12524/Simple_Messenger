9f91e4e6b9b73defa735a4200429f9f265955a95
daria smirnova12524 <d.smirnova@lab244.ru>
2024-06-10 22:11:44 +0300

    second commit: added program code

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,33 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+    public static Socket client;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    private static BufferedReader reader;
+    public static void main(String[] args){
+        try{
+            try{
+            client = new Socket("localhost",4004);
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            System.out.println("Напишите что-нибудь");
+            String phrase = reader.readLine();
+            out.write(phrase+"\n");
+            out.flush();
+            String serverPhrase = in.readLine();
+            System.out.println(serverPhrase);
+            } finally {
+                System.out.println("клиент закрыт");
+                client.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class Server {
-
+    public static ServerSocket server;
+    public static Socket client;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    public static void main(String [] args){
+        try{
+            try{
+                server = new ServerSocket(4004);
+                System.out.println("сервер запущен");
+                client = server.accept();
+                try{
+                    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                    String phrase = in.readLine();
+                    System.out.println(phrase);
+                    out.write("сервер: вы написали:" + phrase + "\n");
+                    out.flush();
+                } finally {
+                    client.close();
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("сервер закрыт");
+                server.close();
+            }
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
 }
