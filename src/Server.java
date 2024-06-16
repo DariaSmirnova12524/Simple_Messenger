67c461ca598b27a2392276514135507764c77a69
daria smirnova12524 <d.smirnova@lab244.ru>
2024-06-16 11:55:06 +0300

    replaced while(true) in server

--- src/Server.java
+++ src/Server.java
@@ -8,38 +8,34 @@ public class Server {
     private static BufferedWriter out;
     public static String phrase;
     public static void main(String [] args){
-
             try {
-                while(true) {
                     try {
-                        server = new ServerSocket(4004);
-                        System.out.println("сервер запущен");
-                        client = server.accept();
-                        try {
-                            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                            out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                            phrase = in.readLine();
-                            System.out.println(phrase);
-                            out.write("сервер: вы написали:" + phrase + "\n");
-                            out.flush();
-                        } finally {
-                            client.close();
-                            in.close();
-                            out.close();
+                        while(true) {
+                            server = new ServerSocket(4004);
+                            System.out.println("сервер запущен");
+                            client = server.accept();
+                            try {
+                                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                                out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                                phrase = in.readLine();
+                                System.out.println(phrase);
+                                out.write("сервер: вы написали:" + phrase + "\n");
+                                out.flush();
+                            } finally {
+                                client.close();
+                                in.close();
+                                out.close();
+                            }
+                            if(phrase.equals(".")){
+                                break;
+                            }
                         }
-
                     } finally {
                         System.out.println("сервер закрыт");
                         server.close();
                     }
-                    if(phrase.equals(".")){
-                        break;
-                    }
-                }
             } catch (IOException e) {
                 System.err.println(e);
             }
-
-
     }
 }
