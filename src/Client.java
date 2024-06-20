f7b0eac99db4923169fbd451fa86120f92b61004
daria smirnova12524 <d.smirnova@lab244.ru>
2024-06-20 22:57:07 +0300

    debug code

--- src/Client.java
+++ src/Client.java
@@ -2,41 +2,44 @@ import Messages.SendingMessagesClient;
 
 import java.io.*;
 import java.net.Socket; //импорт
+import java.util.Scanner;
 
 public class Client {
     public static Socket client; //объявление
-    /*private static BufferedReader in;
-    private static BufferedWriter out; */
     private static ObjectInput in;
     private static ObjectOutput out;
-    /*private static BufferedReader reader;
-    public static String phrase;*/
+    public static String text;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EOFException {
         try {
             try {
-                client = new Socket("localhost", 4004);
-                //reader = new BufferedReader(new InputStreamReader(System.in));
-                out = new ObjectOutputStream(client.getOutputStream());
-                in = new ObjectInputStream(client.getInputStream());
-                SendingMessagesClient message = new SendingMessagesClient("a", "b", "hello!");
-                // while (true) { //для общения до определенной команды
-                System.out.println("User login 1 - " + message.getSenderLogin());
-                System.out.println("User login 2 - " + message.getRecipientLogin());
-                System.out.println("Message - " + message.getMessageText());
-                /*in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                System.out.println("Напишите что-нибудь");
-                phrase = reader.readLine();
-                out.write(phrase + "\n");*/
-                out.writeObject(message);
-                out.flush();
-                /*String serverPhrase = in.readLine();
-                System.out.println(serverPhrase);
-                if (phrase.equals(".")) {
-                  break;
-                 }
-                 }*/
+                try {
+                    client = new Socket("localhost", 4004);
+                } catch (IOException e) {
+                    System.out.println("Сервер занят");
+                }
+                try {
+                    while (true) {
+                        out = new ObjectOutputStream(client.getOutputStream());
+                        in = new ObjectInputStream(client.getInputStream());
+                        System.out.println("Напиши что-нибудь...");
+                        Scanner sc = new Scanner(System.in);
+                        text = sc.nextLine();
+                        SendingMessagesClient message = new SendingMessagesClient("a", "b", text);
+                        System.out.println("User login 1 - " + message.getSenderLogin());
+                        System.out.println("User login 2 - " + message.getRecipientLogin());
+                        System.out.println("Message - " + message.getMessageText());
+                        out.writeObject(message);
+                        out.flush();
+                        if (text.equals(".")) {
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println("Ошибка в сообщении или его выводе");
+                }
+            } catch (NullPointerException n) {
+                System.out.println("Клиент не инициализирован");
             } finally {
                 System.out.println("клиент закрыт");
                 client.close();
@@ -45,8 +48,27 @@ public class Client {
             }
         } catch (IOException e) {
             System.err.println(e);
+        } catch (NullPointerException e) {
+            System.out.println("Клиент не инициализирован!");
         }
 
     }
 
 }
+/*private static BufferedReader in;
+    private static BufferedWriter out; */
+/*private static BufferedReader reader;
+    public static String phrase;*/
+//reader = new BufferedReader(new InputStreamReader(System.in));
+// /*in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//                out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+//                System.out.println("Напишите что-нибудь");
+//                phrase = reader.readLine();
+//                out.write(phrase + "\n");*/
+     /*String serverPhrase = in.readLine();
+                System.out.println(serverPhrase);
+                if (phrase.equals(".")) {
+                  break;
+                 }
+                 }*/
+// while (true) { //для общения до определенной команды
--- src/Server.java
+++ src/Server.java
@@ -7,41 +7,48 @@ import java.net.Socket;
 public class Server {
     public static ServerSocket server;
     public static Socket client;
-    /*private static BufferedReader in;
-    private static BufferedWriter out; */
     private static ObjectInput in;
     private static ObjectOutput out;
-   // public static String phrase;
+    public static String text;
 
     public static void main(String[] args) throws ClassNotFoundException {
         try {
             try {
-                //while (true) {
                 try {
-                    /*  in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));*/
-                    server = new ServerSocket(4004);
-                    System.out.println("сервер запущен");
-                    client = server.accept();
-                    out = new ObjectOutputStream(client.getOutputStream());
-                    in = new ObjectInputStream(client.getInputStream());
-                    SendingMessagesClient message = (SendingMessagesClient) in.readObject();
-                    System.out.println("User login 1 - " + message.getSenderLogin());
-                    System.out.println("User login 2 - " + message.getRecipientLogin());
-                    System.out.println("Message - " + message.getMessageText());
-                    /*phrase = in.readLine();
-                    System.out.println(phrase);
-                    out.write("сервер: вы написали:" + phrase + "\n");*/
-                    out.flush();
+                    try {
+                        server = new ServerSocket(4004);
+                        System.out.println("сервер запущен");
+                    } catch (IOException e) {
+                        System.out.println("Сервер не запущен, порт занят");
+                    }
+                    while (true) {
+                        try {
+                            client = server.accept();
+                        } catch (IOException e) {
+                            System.out.println("клиент не принят");
+                        }
+                        try {
+                            out = new ObjectOutputStream(client.getOutputStream());
+                            in = new ObjectInputStream(client.getInputStream());
+                            SendingMessagesClient message = (SendingMessagesClient) in.readObject();
+                            text = in.readLine();
+                            System.out.println("User login 1 - " + message.getSenderLogin());
+                            System.out.println("User login 2 - " + message.getRecipientLogin());
+                            System.out.println("Message - " + message.getMessageText());
+                            out.flush();
+                        } catch (IOException e) {
+                            System.out.println("ошибка в чтении сообщения!");
+                        }
+                        if (text.equals(".")) {
+                            break;
+                        }
+                    }
                 } finally {
                     client.close();
                     in.close();
                     out.close();
                 }
-                /* if (phrase.equals(".")) {
-                    break;
-                 }
-                }*/
+
             } finally {
                 System.out.println("сервер закрыт");
                 server.close();
@@ -51,3 +58,12 @@ public class Server {
         }
     }
 }
+/*private static BufferedReader in;
+    private static BufferedWriter out; */
+// public static String phrase;
+ /*  in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));*/
+ /*phrase = in.readLine();
+                    System.out.println(phrase);
+                    out.write("сервер: вы написали:" + phrase + "\n");*/
+//
