2b40b4894f1a7ecf28f597d71f1b836451e79043
daria smirnova12524 <d.smirnova@lab244.ru>
2024-06-30 16:10:50 +0300

    added a constructor to the user class

--- src/User.java
+++ src/User.java
@@ -2,4 +2,9 @@ public class User {
     public String login;
     public String password;
     public String phoneNumber;
+    public User(String username, String password,String phoneNumber){
+        username = login;
+        password = this.password;
+        phoneNumber = this.phoneNumber;
+    }
 }
